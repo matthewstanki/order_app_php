24a8ac3a0b8de9e6ab9ff088c54b8f0b3a609aaf
Mateusz Stankiewicz <mateuszstankiewicz999@gmail.com>
2022-02-18 16:04:24 +0100

    bootstrap update and cleaning the code

--- index.php
+++ index.php
@@ -46,11 +46,14 @@ if(isset($_GET["action"]))
 }
 ?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<link href="/dashboard/stylesheets/all.css" rel="stylesheet" type="text/css" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Order_app_php</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<link href="/styles/all.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
 		<div class="main-container" style="height: 100%; display: flex;">
@@ -64,7 +67,7 @@ if(isset($_GET["action"]))
 					while($row = mysqli_fetch_array($result))
 					{
 				?>
-			<div class="col-md-5">
+			<div class="btn-group mr-2">
 				<form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
 					<div style="padding:5px;" align="center">
 						<input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
@@ -73,7 +76,7 @@ if(isset($_GET["action"]))
 						<input type="submit" name="add_to_cart" class="btn btn-primary" value="<?php echo $row["name"]; ?>" />
 					</div>
 				</form>
-			</div>
+					</div>
 			<?php
 					}
 				}
@@ -100,7 +103,7 @@ if(isset($_GET["action"]))
 						<td><?php echo $values["item_name"]; ?></td>
 						<td><?php echo $values["item_time"]; ?></td>
 						<td><?php echo $values["item_status"]; ?></td>
-						<td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><button type="button" class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-remove"></span></button></a></td>
+						<td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><button type="button" class="btn btn-secondary btn-square-sm">X</button></a></td>
 					</tr>
 					<?php
 						}
@@ -115,33 +118,15 @@ if(isset($_GET["action"]))
 	</div>
 	<br />
 	<footer>
-    <div class="row">
-      <div class="large-12 columns">
-        <div class="row">
-          <div class="large-8 columns">
-            <ul class="social">
-              <li class="twitter"><a href="https://twitter.com/apachefriends">Follow us on Twitter</a></li>
-              <li class="facebook"><a href="https://www.facebook.com/we.are.xampp">Like us on Facebook</a></li>
-              <li class="google"><a href="https://plus.google.com/+xampp/posts">Add us to your G+ Circles</a></li>
-            </ul>
-
-            <ul class="inline-list">
-              <li><a href="https://www.apachefriends.org/blog.html">Blog</a></li>
-              <li><a href="https://www.apachefriends.org/privacy_policy.html">Privacy Policy</a></li>
-              <li>
-                <a target="_blank" href="http://www.fastly.com/">                    CDN provided by
-                  <img width="48" data-2x="/dashboard/images/fastly-logo@2x.png" src="/dashboard/images/fastly-logo.png" />
-                </a>
-              </li>
-            </ul>
-          </div>
           <div class="large-4 columns">
             <a target="_blank" href="https://matthewstanki.github.io/"><p class="text-right">Copyright (c) 2022, Mateusz Stankiewicz</p></a>
           </div>
         </div>
       </div>
     </div>
-	</body>
+	<!-- JavaScripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>	
+</body>
   </footer>
 </html>
 
